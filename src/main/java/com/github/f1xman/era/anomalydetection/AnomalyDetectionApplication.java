b1e80640e3bb9cc5353b7391cb160fd55ef54d1b
Tymur Yarosh <tyarosh@intellias.com>
2022-03-16 13:48:52 +0200

    Initial commit

---
+++ src/main/java/com/github/f1xman/era/anomalydetection/AnomalyDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.github.f1xman.era.anomalydetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomalyDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnomalyDetectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/f1xman/era/anomalydetection/AnomalyDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.f1xman.era.anomalydetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnomalyDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
